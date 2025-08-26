1f482bf4bcfe9ea951409a720207b715ff7dceef
Carl Nelson <99733202+CNelson0306@users.noreply.github.com>
2025-08-26 09:19:52 +0100

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,12 +16,12 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMAAAADACAYAAABS3GwHAAAABHNCSVQICAgIfAhkiAAAABl0RVh0U29mdHdhcmUAUGFpbnQuTkVUIHYzLjM2qefiJQAAAEBJREFUeJztwTEBAAAAwqD1T20ND6AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPA5AxUAAZg2VcgAAAAASUVORK5CYII=",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/pwa-512x512.png",
+            src: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAgAAAAIACAYAAAD0eNT6AAAABHNCSVQICAgIfAhkiAAAABl0RVh0U29mdHdhcmUAUGFpbnQuTkVUIHYzLjM2qefiJQAAAEBJREFUeJztwTEBAAAAwqD1T20ND6AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPA5AxUAAZg2VcgAAAAASUVORK5CYII=",
             sizes: "512x512",
             type: "image/png",
           },
